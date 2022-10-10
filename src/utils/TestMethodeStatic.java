3a2cfe087fbcf6ef5d83eb6ef188d0f1ffb720c7
Clément SIMON <simon.clement.pierre.daniel.com>
2022-10-10 16:21:59 +0200

    TP4 Méthodes static

---
+++ src/utils/TestMethodeStatic.java
@@ -0,0 +1,14 @@
+package utils;
+
+public class TestMethodeStatic {
+
+	public static void main(String[] args) {
+		String chaine= "12";
+		int entier = Integer.parseInt(chaine);
+		
+		int a = 2;
+		int b = 10;
+		System.out.println(Integer.max(a, b));
+	}
+
+}
